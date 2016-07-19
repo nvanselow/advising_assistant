937f7a31e1ba29f9651eb25e96933401542dfda6
Nick(Mac) <nvanselow@hotmail.com>
2016-07-19 12:26:31 -0400

    Fix hound violations

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -8,8 +8,8 @@ module DeviseHelper
     messages = messages.join
 
     sentence = I18n.t("errors.messages.not_saved",
-                      :count => resource.errors.count,
-                      :resource => resource.class.model_name.human.downcase)
+                      count: resource.errors.count,
+                      resource: resource.class.model_name.human.downcase)
 
     html = <<-HTML
     <div id="error_explanation">
