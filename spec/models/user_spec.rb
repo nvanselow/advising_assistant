8fbc97746f9fd9f3cd2cc30db1856a6d1d901c8b
Nick(Mac) <nvanselow@hotmail.com>
2016-07-19 11:01:13 -0400

    Add first and last name to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 describe User, type: :model do
+  it { should have_valid(:first_name).when('Bob', 'Susie') }
+  it { should_not have_valid(:first_name).when('', nil) }
+
+  it { should have_valid(:first_name).when('Smith', 'Weeks') }
+  it { should_not have_valid(:first_name).when('', nil) }
 end
