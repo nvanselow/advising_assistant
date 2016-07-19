c114fed815fdba0ab9891e04387996534388ae2a
Nick(Mac) <nvanselow@hotmail.com>
2016-07-19 11:03:32 -0400

    Add first and last name to user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |num| "user_#{num}@mailinator.com" }
     password 'password'
+    sequence(:first_name) { |num| "Bob #{num}" }
+    last_name 'Smith'
   end
 end
