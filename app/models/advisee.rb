fef8affc2994f2c06be9a52ea53d4ca9236cbaf1
Nick(Mac) <nvanselow@hotmail.com>
2016-07-19 15:36:53 -0400

    Use semesters lib instead of array

--- app/models/advisee.rb
+++ app/models/advisee.rb
@@ -6,9 +6,8 @@ class Advisee < ActiveRecord::Base
   validates :email, presence: true,
                     format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
                               message: 'That is not a valid email address' }
-  valid_semesters = %w(Fall Spring Winter Summer)
   validates :graduation_semester, presence: true,
-                                  inclusion: { in: valid_semesters }
+                                  inclusion: { in: Semesters.all }
   validates :graduation_year, numericality: { only_integer: true,
                                               greater_than: 1990,
                                               less_than: 9999 }
