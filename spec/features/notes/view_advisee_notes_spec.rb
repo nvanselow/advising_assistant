8905a98a13010280040dca8d74b7f1eca405be8f
Nick(Mac) <nvanselow@hotmail.com>
2016-07-20 14:39:08 -0400

    Add story and ac to test

---
+++ spec/features/notes/view_advisee_notes_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'User can view notes for an advisee', %{
+  As an advisor
+  I want to see notes for one of my advisees
+  So that I can review the notes I have left for that advisee
+} do
+  # ACCEPTANCE CRITERIA
+  # [ ] On the details page for an advisee, I can see all the notes I have left for that advisee
+  # [ ] Notes are ordered with the most recent at the top
+
+  
+end
