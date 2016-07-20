854fc22199ef8b8152eaa65b287de0f6f36a8151
Nick(Mac) <nvanselow@hotmail.com>
2016-07-19 22:58:27 -0400

    Add controller tests for delete routes

---
+++ spec/controllers/advisees_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe AdviseesController, type: :controller do
+  describe 'DELETE /advisees/:advisee_id' do
+    let(:user) { FactoryGirl.create(:user) }
+
+    before do
+      sign_in user
+    end
+
+    it 'deletes and advisee' do
+      advisee = FactoryGirl.create(:advisee)
+
+      delete :destroy, id: advisee.id
+
+      expect(Advisee.all.count).to eq(0)
+    end
+  end
+end
