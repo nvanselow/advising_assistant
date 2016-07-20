da1801898b6c300f25cbe29221a965359a07f96b
Nick(Mac) <nvanselow@hotmail.com>
2016-07-20 10:16:38 -0400

    Add search scope

--- app/models/advisee.rb
+++ app/models/advisee.rb
@@ -1,5 +1,9 @@
 class Advisee < ActiveRecord::Base
+  include PgSearch
+
   belongs_to :user
+
+  pg_search_scope :search_advisees, against: [:first_name, :last_name, :email]
 
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -12,9 +16,12 @@ class Advisee < ActiveRecord::Base
                                               greater_than: 1990,
                                               less_than: 9999 }
 
+  scope :search, -> (query) { search_advisees(query) if query.present? }
+
   def self.all_for(user)
     Advisee.where(user: user)
   end
+
 
   def full_name
     "#{first_name} #{last_name}"
