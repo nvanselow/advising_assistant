a45332359453d73b0af30caa70f131565dbe8537
Nick(Mac) <nvanselow@hotmail.com>
2016-07-20 17:40:14 -0400

    Allow status code to be passed in

--- spec/support/test_helpers.rb
+++ spec/support/test_helpers.rb
@@ -1,9 +1,9 @@
-def expect_json_response(response)
+def expect_json_response(response, status_code)
   expect(response.content_type).to eq('application/json')
-  expect(response).to have_http_status(:ok)
+  expect(response).to have_http_status(status_code)
 end
 
-def parse_json(response)
-  expect_json_response(response)
+def parse_json(response, status_code = :ok)
+  expect_json_response(response, status_code)
   JSON.parse(response.body)
 end
