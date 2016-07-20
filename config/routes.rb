4389622bbca147694cd7ab1c3b1a898db3dc1da1
Nick(Mac) <nvanselow@hotmail.com>
2016-07-20 17:41:22 -0400

    Create advisee_notes api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :search_advisees, only: [:index]
+
+      resources :advisees, only: [] do
+        resources :advisee_notes, only: [:index, :create]
+      end
+
+      resources :advisee_notes, only: [:update, :destroy]
     end
   end
 end
