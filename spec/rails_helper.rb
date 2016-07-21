5a7a71f3a2752f0d517b16b6fd67ed19b1905c73
Nick(Mac) <nvanselow@hotmail.com>
2016-07-20 21:48:21 -0400

    Add poltergeist

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'capybara/poltergeist'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -27,7 +28,8 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-Capybara.javascript_driver = :webkit
+# Capybara.javascript_driver = :webkit
+Capybara.javascript_driver = :poltergeist
 Capybara::Webkit.configure do |config|
   config.block_unknown_urls
 end
