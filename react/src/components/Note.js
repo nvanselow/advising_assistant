1821103eec955ba6edac400cc37d2a899092182a
Nick(Mac) <nvanselow@hotmail.com>
2016-07-21 09:24:58 -0400

    Note deletion working

--- react/src/components/Note.js
+++ react/src/components/Note.js
@@ -1,13 +1,36 @@
 import React, { Component, PropTypes } from 'react';
+import Confirmation from '../lib/Confirmation';
 
 class Note extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleDelete = this.handleDelete.bind(this);
+    this.deleteNoteConfirmed = this.deleteNoteConfirmed.bind(this);
+  }
+
+  handleDelete() {
+    Confirmation.show({
+                        header: 'Delete this note?',
+                        okText: 'Yes, Delete Note',
+                        okCallback: this.deleteNoteConfirmed
+                      });
+  }
+
+  deleteNoteConfirmed() {
+    this.props.onDelete(this.props.note);
+  }
+
   render() {
     let note = this.props.note;
 
     return (
       <div className="note">
-      <h4>{note.updated_at}</h4>
+        <h4>{note.updated_at}</h4>
         <p>{note.body}</p>
+        <button className="btn-floating danger" onClick={this.handleDelete}>
+          <i className="material-icons">delete</i>
+        </button>
       </div>
     );
   }
