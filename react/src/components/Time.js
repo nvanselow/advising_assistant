0b20e611119d4c00a89a5908f2e7e526ae56e2d6
Nick(Mac) <nvanselow@hotmail.com>
2016-07-21 15:06:22 -0400

    Create generic time component

---
+++ react/src/components/Time.js
@@ -0,0 +1,38 @@
+import React, { Component, PropTypes } from 'react';
+import moment from 'moment-timezone/builds/moment-timezone-with-data';
+
+class Time extends Component {
+  constructor(props) {
+    super(props);
+
+    if (!('displayType' in props)){
+      this.displayType = 'standard';
+    } else {
+      this.displayType = props.displayType;
+    }
+
+    this.transformTime = this.transformTime.bind(this);
+  }
+
+  transformTime() {
+    let dateTime = moment(this.props.dateTime);
+    if (this.displayType == 'timeago') {
+      return dateTime.tz(moment.tz.guess()).fromNow();
+    } else {
+      return dateTime.tz(moment.tz.guess()).format('MMM Qo, YYYY h:mm a');
+    }
+  }
+
+  render() {
+    return (
+      <span>{this.transformTime()}</span>
+    );
+  }
+}
+
+Time.propTypes = {
+  dateTime: PropTypes.string.isRequired,
+  displayType: PropTypes.string
+}
+
+export default Time;
