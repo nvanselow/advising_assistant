8a5aa479602a226df03793e720561e2d0249ae6e
Nick(Mac) <nvanselow@hotmail.com>
2016-07-22 11:06:14 -0400

    Fix timezone attribute

--- spec/factories/meeting.rb
+++ spec/factories/meeting.rb
@@ -3,7 +3,11 @@ FactoryGirl.define do
     sequence(:description) { |num| "Meeting #{num}" }
     start_time '2016-01-31 20:00'
     end_time '2016-01-31 21:00'
-    time_zone 'Eastern Time (US & Canada)'
+    timezone 'Eastern Time (US & Canada)'
     advisee
+
+    factory :meeting_with_duration do
+      duration 60
+    end
   end
 end
