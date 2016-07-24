2ba0ba18d1a1ec718105b04266890c7f2f9ca8d5
Nick(Mac) <nvanselow@hotmail.com>
2016-07-24 01:58:30 -0400

    fix scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -14,8 +14,6 @@ end
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :microsoft_office365, ENV['OFFICE365_KEY'], ENV['OFFICE365_SECRET'],
   {
-    scope: [
-      'https://outlook.office.com/calendars.readwrite'
-    ]
+    scope: 'openid email profile https://outlook.office.com/calendars.readwrite'
   }
 end
