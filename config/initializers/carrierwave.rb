b8c6289a5126f86a645a07795d52b96e82e1fe61
Nick(Mac) <nvanselow@hotmail.com>
2016-07-24 17:27:52 -0400

    extra space

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,6 @@ CarrierWave.configure do |config|
       aws_access_key_id: ENV["AWS_PUBLIC_KEY"],
       aws_secret_access_key: ENV["AWS_SECRET_KEY"]
     }
-    config.fog_directory  = ENV["S3_BUCKET"]
+    config.fog_directory = ENV["S3_BUCKET"]
   end
 end
