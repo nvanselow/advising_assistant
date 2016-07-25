55ae0fcd2568edc12463adbab526d0830b9f3a43
Nick(Mac) <nvanselow@hotmail.com>
2016-07-25 11:30:46 -0400

    Move class method to top

--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -29,6 +29,27 @@ describe Meeting, type: :model do
     it { should have_many(:notes) }
   end
 
+  describe '.new_from_duration' do
+    it 'sets an end time if a duration is provided' do
+      duration = 90
+      meeting = FactoryGirl.attributes_for(:meeting_with_duration,
+                                           end_time: nil,
+                                           duration: duration)
+      new_meeting = Meeting.new_from_duration(meeting)
+
+      old_start_time = DateTime.parse(meeting[:start_time])
+      expect(new_meeting.end_time).to eq(old_start_time + duration.minutes)
+    end
+
+    it 'does not set the end time if no duration is provided' do
+      meeting = FactoryGirl.attributes_for(:meeting, end_time: nil)
+
+      new_meeting = Meeting.new_from_duration(meeting)
+
+      expect(new_meeting.end_time).to eq(nil)
+    end
+  end
+
   describe '#duration' do
     let(:start_time) { DateTime.new(2016, 01, 01, 11, 0, 0) }
 
@@ -62,27 +83,6 @@ describe Meeting, type: :model do
       meeting = FactoryGirl.build(:meeting, end_time: nil)
 
       expect(meeting.duration).to eq(0)
-    end
-  end
-
-  describe '.new_from_duration' do
-    it 'sets an end time if a duration is provided' do
-      duration = 90
-      meeting = FactoryGirl.attributes_for(:meeting_with_duration,
-                                           end_time: nil,
-                                           duration: duration)
-      new_meeting = Meeting.new_from_duration(meeting)
-
-      old_start_time = DateTime.parse(meeting[:start_time])
-      expect(new_meeting.end_time).to eq(old_start_time + duration.minutes)
-    end
-
-    it 'does not set the end time if no duration is provided' do
-      meeting = FactoryGirl.attributes_for(:meeting, end_time: nil)
-
-      new_meeting = Meeting.new_from_duration(meeting)
-
-      expect(new_meeting.end_time).to eq(nil)
     end
   end
 
