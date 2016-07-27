1bdeb12ec98688eada0667c703e8cffeba314587
Nick(Mac) <nvanselow@hotmail.com>
2016-07-27 10:26:17 -0400

    fix typo

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -6,5 +6,12 @@ class Semester < ActiveRecord::Base
   validates :year, presence: true, numericality: { only_integer: true,
                                                    greater_than: 1990,
                                                    less_than: 9999 }
+  validates :remaining_courses, inclusion: { in: [true, false] }
   validates :graduation_plan, presence: true
+
+  def as_json(options = {})
+    s = super(options)
+    s[:courses] = courses
+    s
+  end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -4,7 +4,7 @@ describe Course, type: :model do
   it { should have_valid(:name).when('PSY 500', 'Course Name') }
   it { should_not have_valid(:name).when('', nil) }
 
-  it { shoud validate_presence_of(:semester) }
+  it { should validate_presence_of(:semester) }
 
   it { should belong_to(:semester) }
 end
