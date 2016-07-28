15928bc65984ca46b9bb231b1b2e826a9b99d5ba
Nick(Mac) <nvanselow@hotmail.com>
2016-07-28 13:28:13 -0400

    hound violations

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -3,7 +3,6 @@ class IdentitiesController < ApplicationController
     Identity.find_or_create_from_omniauth(request.env['omniauth.auth'],
                                           current_user)
 
-
-    redirect_to (stored_location_for(:user) || advisees_path)
+    redirect_to stored_location_for(:user) || advisees_path
   end
 end
