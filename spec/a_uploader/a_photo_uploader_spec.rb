0f9c135ef3c49a4396f3f49aa6fb6ca91d54759b
Nick(Mac) <nvanselow@hotmail.com>
2016-07-28 23:55:47 -0400

    change expectation to actual uploader

--- spec/a_uploader/a_photo_uploader_spec.rb
+++ spec/a_uploader/a_photo_uploader_spec.rb
@@ -4,13 +4,13 @@ require 'pry'
 describe 'AdviseePhotoUploader' do
   it 'calls fog for storage in production' do
     allow(Rails).to receive_message_chain(:env, :test?).and_return(false)
-    expect(CarrierWave::Uploader::Base).to receive(:storage).with(:fog)
+    expect(AdviseePhotoUploader).to receive(:storage).with(:fog)
     load File.expand_path('./app/uploaders/advisee_photo_uploader.rb')
   end
 
   it 'calls file for storage in test' do
     allow(Rails).to receive_message_chain(:env, :test?).and_return(true)
-    expect(CarrierWave::Uploader::Base).to receive(:storage).with(:file)
+    expect(AdviseePhotoUploader).to receive(:storage).with(:file)
     load File.expand_path('./app/uploaders/advisee_photo_uploader.rb')
   end
 end
