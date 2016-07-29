2563ee5feac8b183fb4356ab68546a47feeed612
Nick(Mac) <nvanselow@hotmail.com>
2016-07-28 22:00:16 -0400

    Add Fog mock

--- spec/controllers/advisees_controller_spec.rb
+++ spec/controllers/advisees_controller_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 describe AdviseesController, type: :controller do
+  let(:user) { FactoryGirl.create(:user) }
+
+  before do
+    sign_in user
+  end
+
+  Fog.mock!
+  Fog.credentials_path = Rails.root.join('fog_test_credentials.yml')
+  connection = Fog::Storage.new(:provider => 'AWS')
+  connection.directories.create(:key => 'my_bucket')
+
   describe 'POST /advisees' do
     it 'calls :fog in production for photo upload' do
       allow(Rails).to receive_message_chain(:env, :test?).and_return(false)
@@ -8,18 +19,14 @@ describe AdviseesController, type: :controller do
       advisee = FactoryGirl.attributes_for(:advisee)
 
       post :create, advisee: advisee
+
+      # expect(flash[:success]).to include('Advisee added')
     end
   end
 
   describe 'DELETE /advisees/:advisee_id' do
-    let(:user) { FactoryGirl.create(:user) }
-
-    before do
-      sign_in user
-    end
-
     it 'deletes an advisee' do
-      advisee = FactoryGirl.create(:advisee)
+      advisee = FactoryGirl.create(:advisee, photo: nil)
 
       delete :destroy, id: advisee.id
 
