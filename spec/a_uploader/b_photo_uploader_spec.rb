fdf6166e714be2b9e86dfb48ee1d9fb2c8d306b2
Nick(Mac) <nvanselow@hotmail.com>
2016-07-29 00:38:34 -0400

    trying reloading files

---
+++ spec/a_uploader/b_photo_uploader_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'AdviseePhotoUploader' do
+  it 'calls file for storage in test' do
+    allow(Rails).to receive_message_chain(:env, :test?).and_return(true)
+    expect(AdviseePhotoUploader).to receive(:storage).with(:file)
+    load File.expand_path('./app/uploaders/advisee_photo_uploader.rb')
+  end
+
+  it 'calls fog for storage in production' do
+    allow(Rails).to receive_message_chain(:env, :test?).and_return(false)
+    expect(AdviseePhotoUploader).to receive(:storage).with(:fog)
+    load File.expand_path('./app/uploaders/advisee_photo_uploader.rb')
+  end
+end
