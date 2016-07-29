a521de045062d43b728af377a5996bb9448c10e8
Nick(Mac) <nvanselow@hotmail.com>
2016-07-29 01:11:34 -0400

    try it twice

--- spec/a_uploader/b_photo_uploader_spec.rb
+++ spec/a_uploader/b_photo_uploader_spec.rb
@@ -8,9 +8,6 @@ describe 'AdviseePhotoUploader' do
   end
 
   it 'calls fog for storage in production' do
-    SimpleCov.start do
-      command_name 'fog-test'
-    end
     allow(Rails).to receive_message_chain(:env, :test?).and_return(false)
     expect(AdviseePhotoUploader).to receive(:storage).with(:fog)
 
@@ -20,6 +17,13 @@ describe 'AdviseePhotoUploader' do
   it 'calls file for storage in test' do
     allow(Rails).to receive_message_chain(:env, :test?).and_return(true)
     expect(AdviseePhotoUploader).to receive(:storage).with(:file)
+    load File.expand_path('./app/uploaders/advisee_photo_uploader.rb')
+  end
+
+  it 'calls fog for storage in production' do
+    allow(Rails).to receive_message_chain(:env, :test?).and_return(false)
+    expect(AdviseePhotoUploader).to receive(:storage).with(:fog)
+
     load File.expand_path('./app/uploaders/advisee_photo_uploader.rb')
   end
 end
