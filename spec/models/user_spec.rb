33166f52efcbf23f53ab152ca8920610629ce4d5
Nick(Mac) <nvanselow@hotmail.com>
2016-07-29 08:42:41 -0400

    add test for can_modify?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,21 @@ describe User, type: :model do
   it { should have_many(:advisees).dependent(:destroy) }
   it { should have_many(:identities).dependent(:destroy) }
   it { should have_many(:meetings).dependent(:destroy) }
+
+  describe '#can_modify?' do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:another_user) { FactoryGirl.create(:user) }
+
+    it 'returns true if the user is allowed to modify the resource' do
+      advisee = FactoryGirl.create(:advisee, user: user)
+
+      expect(user.can_modify?(advisee)).to be(true)
+    end
+
+    it 'returns false if the user is not allowed to modify the resource' do
+      advisee = FactoryGirl.create(:advisee, user: another_user)
+
+      expect(user.can_modify?(advisee)).to be(false)
+    end
+  end
 end
