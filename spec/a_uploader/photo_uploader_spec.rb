468fcaf83d87b86b6e2a704ab6e6ff503b99ca5d
Nick(Mac) <nvanselow@hotmail.com>
2016-07-28 23:16:40 -0400

    Fork this test so it doesn't interfere with anything else

--- spec/a_uploader/photo_uploader_spec.rb
+++ spec/a_uploader/photo_uploader_spec.rb
@@ -1,20 +1,22 @@
-require 'spec_helper'
-require 'pry'
-describe 'AdviseePhotoUploader' do
-  it 'calls fog for storage in production' do
-    foo = Class.new do
-      def self.env
-      end
+fork{
+  require 'spec_helper'
 
-      def self.storage(_type)
+  describe 'AdviseePhotoUploader' do
+    it 'calls fog for storage in production' do
+      foo = Class.new do
+        def self.env
+        end
+
+        def self.storage(_type)
+        end
       end
+      stub_const('Rails', foo)
+      stub_const('CarrierWave', foo)
+      stub_const('CarrierWave::Uploader::Base', foo)
+      stub_const('Rails::Uploader', foo)
+      stub_const('Rails::Base', foo)
+      allow(Rails).to receive_message_chain(:env, :test?).and_return(false)
+      require_relative '../../app/uploaders/advisee_photo_uploader'
     end
-    stub_const('Rails', foo)
-    stub_const('CarrierWave', foo)
-    stub_const('CarrierWave::Uploader::Base', foo)
-    stub_const('Rails::Uploader', foo)
-    stub_const('Rails::Base', foo)
-    allow(Rails).to receive_message_chain(:env, :test?).and_return(false)
-    require_relative '../../app/uploaders/advisee_photo_uploader'
-  end
-end
+  end  
+}
