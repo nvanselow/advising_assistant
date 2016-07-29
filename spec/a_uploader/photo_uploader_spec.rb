44195e9352895c0e1e68e18db41873b06849323a
Nick(Mac) <nvanselow@hotmail.com>
2016-07-28 23:11:28 -0400

    Fix quotes

--- spec/a_uploader/photo_uploader_spec.rb
+++ spec/a_uploader/photo_uploader_spec.rb
@@ -9,11 +9,11 @@ describe 'AdviseePhotoUploader' do
       def self.storage(_type)
       end
     end
-    stub_const("Rails", foo)
-    stub_const("CarrierWave", foo)
-    stub_const("CarrierWave::Uploader::Base", foo)
-    stub_const("Rails::Uploader", foo)
-    stub_const("Rails::Base", foo)
+    stub_const('Rails', foo)
+    stub_const('CarrierWave', foo)
+    stub_const('CarrierWave::Uploader::Base', foo)
+    stub_const('Rails::Uploader', foo)
+    stub_const('Rails::Base', foo)
     allow(Rails).to receive_message_chain(:env, :test?).and_return(false)
     require_relative '../../app/uploaders/advisee_photo_uploader'
   end
