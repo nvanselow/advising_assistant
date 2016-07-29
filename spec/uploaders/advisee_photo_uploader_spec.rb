51c8be8f7b27b5f155c3660f2bc59efe602eee93
Nick(Mac) <nvanselow@hotmail.com>
2016-07-28 22:26:40 -0400

    Add test for calling storage method with fog

--- spec/uploaders/advisee_photo_uploader_spec.rb
+++ spec/uploaders/advisee_photo_uploader_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'AdviseePhotoUploader' do
   it 'sets the storage to fog if in development or production' do
     allow(Rails).to receive_message_chain(:env, :test?).and_return(false)
+    expect(CarrierWave::Uploader::Base).to receive(:storage).with(:fog)
     uploader = AdviseePhotoUploader.new
   end
 end
