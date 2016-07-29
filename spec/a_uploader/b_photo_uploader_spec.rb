b26579011c491998b98f1918bb3edd878f9381eb
Nick(Mac) <nvanselow@hotmail.com>
2016-07-29 00:58:56 -0400

    try running the test twice

--- spec/a_uploader/b_photo_uploader_spec.rb
+++ spec/a_uploader/b_photo_uploader_spec.rb
@@ -12,4 +12,10 @@ describe 'AdviseePhotoUploader' do
     expect(AdviseePhotoUploader).to receive(:storage).with(:fog)
     load File.expand_path('./app/uploaders/advisee_photo_uploader.rb')
   end
+
+  it 'calls file for storage in test' do
+    allow(Rails).to receive_message_chain(:env, :test?).and_return(true)
+    expect(AdviseePhotoUploader).to receive(:storage).with(:file)
+    load File.expand_path('./app/uploaders/advisee_photo_uploader.rb')
+  end
 end
