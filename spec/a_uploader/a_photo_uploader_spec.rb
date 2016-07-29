9af1bd1141a6bedab4450eb6abe898665c8b7bdf
Nick(Mac) <nvanselow@hotmail.com>
2016-07-28 23:43:46 -0400

    add test for calling file

--- spec/a_uploader/a_photo_uploader_spec.rb
+++ spec/a_uploader/a_photo_uploader_spec.rb
@@ -7,4 +7,10 @@ require 'pry'
       expect(CarrierWave::Uploader::Base).to receive(:storage).with(:fog)
       load File::expand_path('./app/uploaders/advisee_photo_uploader.rb')
     end
+
+    it 'calls file for storage in test' do
+      allow(Rails).to receive_message_chain(:env, :test?).and_return(true)
+      expect(CarrierWave::Uploader::Base).to receive(:storage).with(:file)
+      load File::expand_path('./app/uploaders/advisee_photo_uploader.rb')
+    end
   end
